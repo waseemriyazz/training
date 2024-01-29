1080dd50d61aa9c8c144ff82394d2357d3de2683
waseem-avi <waseem.riyaz@avisoft.io>
2024-01-29 14:09:00 +0530

    unique_in_twice

---
+++ 29 jan/unique_in_twice.py
@@ -0,0 +1,12 @@
+# size has to be even 
+# only one number should be unique
+size = int(input("Enter the number of elements "))
+myList = []
+
+for i in range(0, size):
+    i = int(input())
+    myList.append(i)
+answer = 0 
+for i in myList:
+    answer = answer ^ i
+print(answer)
