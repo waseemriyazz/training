4570c197f3cc3692e27281e09d6d1ae4d12947ca
waseem-avi <waseem.riyaz@avisoft.io>
2024-01-29 14:48:39 +0530

    query

---
+++ 29 jan/query.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the value of N"))
+q = int(input("Enter the value of Q"))
+if q==1:
+    sum = 0
+    for i in range(1, n+1):
+        sum = sum + i
+        print(sum)
+if q==2:
+    pro = 1 
+    for i in range ( 1 , n+1):
+        pro = pro * i 
+        print(pro)
