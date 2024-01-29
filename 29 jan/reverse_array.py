3483fe52818413c22e66d6cbd04c0915e092ec9b
waseem-avi <waseem.riyaz@avisoft.io>
2024-01-29 13:57:58 +0530

    reverse_array from a position

---
+++ 29 jan/reverse_array.py
@@ -0,0 +1,17 @@
+size = int(input("Enter the number of elements "))
+myList = []
+
+for i in range(0, size):
+    i = int(input())
+    myList.append(i)
+    
+pos = int(input("Enter the position "))
+j = size-1
+for i in range(pos, size):
+    if j>i:
+        myList[i], myList[j]= myList[j], myList[i]
+        j= j-1
+    else:
+        break
+       
+print(myList)
