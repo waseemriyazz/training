154ec77e4a9dd7eb6a306de571d3290aed1d9980
waseem-avi <waseem.riyaz@avisoft.io>
2024-01-30 14:56:04 +0530

    incomplete code

---
+++ 29 jan/sum_infinite.py
@@ -0,0 +1,12 @@
+size = int(input("Enter the size of array "))
+myList = []
+
+for i in range(0, size):
+    i = int(input("Enter the {} element ".format(i+1)))
+    myList.append(i)
+
+right = int(input("Enter the value of right "))
+left = int(input("Enter the value of right "))
+sum = 0
+for i in range(0, right):
+    
