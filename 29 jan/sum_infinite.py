e224f68bcd657a4a123db03e80cf2741275b1ef1
waseem-avi <waseem.riyaz@avisoft.io>
2024-01-30 15:15:58 +0530

    solved sum infinite

--- 29 jan/sum_infinite.py
+++ 29 jan/sum_infinite.py
@@ -6,7 +6,12 @@ for i in range(0, size):
     myList.append(i)
 
 right = int(input("Enter the value of right "))
-left = int(input("Enter the value of right "))
-sum = 0
+left = int(input("Enter the value of left "))
+sumr = 0
+suml = 0
 for i in range(0, right):
+    sumr = sumr + myList[ i % len(myList) ]
+for i in range(0, left):
+    suml= suml + myList[i % len(myList) ]
     
+print(sumr-suml)
