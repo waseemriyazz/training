41d8ec49835ba149dc51156340fd620677fb9686
waseem-avi <waseem.riyaz@avisoft.io>
2024-01-30 15:50:13 +0530

    solved longest sequence

--- 29 jan/longest_consective.py
+++ 29 jan/longest_consective.py
@@ -5,3 +5,15 @@ for i in range(0, size):
     i = int(input("Enter the {} element ".format(i+1)))
     myList.append(i)
 myList.sort()
+longest = 0 
+curr = 0
+key = myList[0]
+for i in range(1, len(myList)):
+    if  myList[i] - key == 1: 
+        curr = curr + 1
+        longest = max(curr, longest)
+        key = myList[i]
+    else :
+        length = 0
+    
+print(longest+1)
