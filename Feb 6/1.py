f191462ed993768765edbb0120d1174eb1f704e6
waseem-avi <waseem.riyaz@avisoft.io>
2024-02-06 13:01:10 +0530

    solved 1

--- Feb 6/1.py
+++ Feb 6/1.py
@@ -1,1 +1,7 @@
 # 1.Write a Python program to copy the contents of a file to another file
+
+with open("Feb 6/1_file_1.txt", "r") as source:
+    content = source.read()
+
+with open("Feb 6/1_file_2.txt", "w") as dest:
+    dest.write(content)
