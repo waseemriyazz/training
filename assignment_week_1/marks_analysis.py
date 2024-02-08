13d195e5d1d904c4749690562ffe33cf864c1399
waseem-avi <waseem.riyaz@avisoft.io>
2024-02-08 13:02:18 +0530

    handled various cases of exceptional inputs

--- assignment_week_1/marks_analysis.py
+++ assignment_week_1/marks_analysis.py
@@ -14,14 +14,45 @@ class Student:
         if self.physics < 33 or self.chemistry < 33 or self.maths < 33:
             return True
         return False
+    
     def total_marks(self):
         total_marks = self.physics + self.chemistry + self.maths
         return total_marks
+    
     def average(self):
         total = self.total_marks() / 3
         return total
     
+
+def get_sub(subject):
+    try:
+        marks= int(input(f"Enter marks in {subject}"))
+        return marks
+    except:
+        print("Enter an integer value")
+        return get_sub(subject)
+    
+
+def get_min():
+    try:
+        min_threshold = int(input("Enter the value of minimum threshold : "))
+        return min_threshold
+    except:
+        print("Enter an integer value")
+        return get_min()
+    
+
+def get_max():
+    try: 
+        max_threshold = int(input("Enter the value of maximum threshold : "))
+        return max_threshold
+    except:
+        print("Enter an integer value")
+        return get_max()
+    
+
 student_list = {}
+
 
 
 class_strength = int(input("Enter the number of students: "))
@@ -32,21 +63,21 @@ for i in range(class_strength):
 
     name = input("Enter student name : ")
 
-    physics = int(input("Enter marks in physics : "))
+    physics = get_sub("physics")
 
-    chemistry = int(input("Enter marks in chemistry : "))
+    chemistry = get_sub("chemistry")
 
-    maths = int(input("Enter marks in maths : "))
+    math = get_sub("maths")
 
-    s = Student(name, physics, chemistry, maths)
+    s = Student(name, physics, chemistry, math)
 
     student_list[i] = s
 
 
+        
+min_threshold = get_min()
+max_threshold = get_max()
 
-min_threshold = int(input("Enter the value of minimum threshold : "))
-
-max_threshold = int(input("Enter the value of maximum threshold : "))
 
 above = []
 
@@ -74,7 +105,7 @@ for student in student_list.values():
     if student.fail == True:
         failed.append(student)
 
-while 1>0:
+while 1 > 0:
     print()
     print("Enter 1 to get list of students above max ")
     print("Enter 2 to get list of students between max and min ")
@@ -83,27 +114,29 @@ while 1>0:
     print("Enter 5 to print the list of students who failed ")
     print("Enter 6 to exit ")
     
-    query = int(input("Enter the analysis you want to perform( 1 , 2 , 3 , 4 , 5 , 6 ): "))
+    try :
+        query = int(input("Enter the analysis you want to perform( 1 , 2 , 3 , 4 , 5 , 6 ): "))
 
-    if query == 1 :
-        for student in above:
-            print(student)
-    elif query == 2 :
-        for student in between:
-            print(student)
-    elif query == 3 :
-        for student in below:
-            print(student)
-    elif query == 4 :
-        for student in attention:
-            print(student)
-    elif query == 5 :
-        for student in failed:
-            print(student)
-    else: 
-        print("exiting operation . . .")
-        break
-        
+        if query == 1 :
+            for student in above:
+                print(student)
+        elif query == 2 :
+            for student in between:
+                print(student)
+        elif query == 3 :
+            for student in below:
+                print(student)
+        elif query == 4 :
+            for student in attention:
+                print(student)
+        elif query == 5 :
+            for student in failed:
+                print(student)
+        else: 
+            print("exiting operation . . .")
+            break
+    except :
+        print("Enter an integer value")    
 
     
 
